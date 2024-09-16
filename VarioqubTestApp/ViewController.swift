3fee89af1c190e70db1f946cc24f28ecf11af1ee
IronMeddie <mikaelofarius@gmail.com>
2024-09-16 17:00:44 +0300

    Initial Commit

---
+++ VarioqubTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VarioqubTestApp
+//
+//  Created by Mikhail Taranyuk on 16.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
